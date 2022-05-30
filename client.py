3e3039d81c2d5b3aa72042b2800064999342bc0e
Xenely14 <83385888+Xenely14@users.noreply.github.com>
2022-05-30 20:02:51 +0800

    Delete client.py

--- client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect(('178.250.158.150', 1337))
-
-while True:
-    server_data = server.recv(65536)
-    if server_data:
-        print(server_data.decode('utf-8'))
